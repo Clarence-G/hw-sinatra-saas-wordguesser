09fb9012f54e81dd214057eda4bf0b92f7a2e9d8
Clarence <yoououyo@qq.com>
2021-10-20 20:23:42 +0800

    finish part4

--- app.rb
+++ app.rb
@@ -40,6 +40,14 @@ class WordGuesserApp < Sinatra::Base
   post '/guess' do
     letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
+    begin
+      result = @game.guess(letter)
+    rescue ArgumentError
+      flash[:message] = "Invalid guess"
+    end
+    if result == false
+      flash[:message] = "You have already used that letter"
+    end
     redirect '/show'
   end
   
