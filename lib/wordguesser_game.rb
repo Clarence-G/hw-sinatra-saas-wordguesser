07095e2979c069b5079fc73996e5ef4d43deac11
Clarence <yoououyo@qq.com>
2021-10-23 16:14:46 +0800

    test local db

--- lib/wordguesser_game.rb
+++ lib/wordguesser_game.rb
@@ -73,13 +73,20 @@ class WordGuesserGame
   # and then running $ irb -I. -r app.rb
   # And then in the irb: irb(main):001:0> WordGuesserGame.get_random_word
   #  => "cooking"   <-- some random word
+  # def self.get_random_word
+  #   require 'uri'
+  #   require 'net/http'
+  #   uri = URI('http://randomword.saasbook.info/RandomWord')
+  #   Net::HTTP.new('randomword.saasbook.info').start { |http|
+  #     return http.post(uri, "").body
+  #   }
+  # end
   def self.get_random_word
-    require 'uri'
-    require 'net/http'
-    uri = URI('http://randomword.saasbook.info/RandomWord')
-    Net::HTTP.new('randomword.saasbook.info').start { |http|
-      return http.post(uri, "").body
-    }
+    require "sqlite3"
+    db = SQLite3::Database.new "lib/words.sqlite3"
+    db.execute "SELECT word from words ORDER BY RANDOM () LIMIT 1" do |s|
+        return s[0]
+    end
   end
 
 end
